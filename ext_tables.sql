c6c902fb023bd92800077c6d467263959e5dfed0
Daniel Klockenkämper <dk@marketing-factory.de>
2017-11-20 16:09:24 +0100

    add name to keys

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,10 +2,10 @@
 # Table structure for table 'prometheus_metrics'
 #
 CREATE TABLE prometheus_metrics (
-	metric_key varchar(128) NOT NULL,
-	metric_value int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
+    metric_key varchar(128) NOT NULL,
+    metric_value int(11) DEFAULT '0' NOT NULL,
+    tstamp int(11) DEFAULT '0' NOT NULL,
 
-	PRIMARY KEY (metric_key),
-	KEY(tstamp)
+    KEY metric_key (metric_key(100)),
+    KEY tstamp (tstamp)
 ) ENGINE=InnoDB;
