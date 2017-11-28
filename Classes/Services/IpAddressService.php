e87f095bf646de0ba76a71cbb8e543323d2437a0
Sebastian Fischer <sf@marketing-factory.de>
2017-11-28 07:52:24 +0000

    Exchange custom get ip method with core functionality

--- Classes/Services/IpAddressService.php
+++ Classes/Services/IpAddressService.php
@@ -12,7 +12,7 @@ class IpAddressService
     public function ipInAllowedRange($address = '')
     {
         if (empty($address)) {
-            $address = $this->getCurrentIpAddress();
+            $address = GeneralUtility::getIndpEnv('REMOTE_ADDR');
         }
 
         $addressAllowed = false;
@@ -27,24 +27,5 @@ class IpAddressService
             }
         }
         return $addressAllowed;
-    }
-
-    /**
-     * @return string
-     */
-    protected function getCurrentIpAddress()
-    {
-        $userIp = $_SERVER['REMOTE_ADDR'];
-
-        $forwardedUserIp = GeneralUtility::trimExplode(';', $_SERVER['HTTP_X_FORWARDED_FOR']);
-        if (!empty($forwardedUserIp)
-            && (preg_match('/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/', $forwardedUserIp[0], $matches))
-        ) {
-            if (!empty($matches[0])) {
-                $userIp = $matches[0];
-            }
-        }
-
-        return $userIp;
     }
 }
